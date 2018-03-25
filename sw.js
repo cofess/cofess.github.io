32e8bf7a88877c8d3f1d7e8eaf144d5a0e12a6f3
cofess <285088180@qq.com>
2018-03-25 15:27:00 +0800

    no message

--- sw.js
+++ sw.js
@@ -24,6 +24,8 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
+  precacheUrls.push('/2018/03/25/年后的状态，懒！.html');
+
   precacheUrls.push('/2017/12/17/visual-studio-code-personal-use-plug-in-colation.html');
 
   precacheUrls.push('/2017/11/01/hexo-blog-theme-pure-usage-description.html');
@@ -31,8 +33,6 @@ var precacheUrls = [];
   precacheUrls.push('/2017/09/06/how-to-use-mathjax-to-render-latex-mathematical-formula-in-hexo.html');
 
   precacheUrls.push('/2017/08/16/comon-plug-in-and-usage-of-hexo-blog.html');
-
-  precacheUrls.push('/2017/08/15/third-party-socialized-review-system.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
